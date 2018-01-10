97e5e63d384247e289594ce6abdd50e3f1696456
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2018-01-10 21:42:07 +0000

    corrected mistakes and updated readme

--- main.js
+++ main.js
@@ -1,61 +1,58 @@
 // Explaining scope functions
+var outsideVariable = 5;
+var insideVariable = 7;
+//
+function doSomething() {
+  console.log(outsideVariable);
+}
 
-// var outsideVariable = 5;
-// var insideVariable = 7;
-//
-// function doSomething() {
-//   console.log(outsideVariable);
-// }
-//
-// doSomething();
+doSomething();
 
 
-// Further Explaining scoping functions
+//Further Explaining scoping functions
+function firstFunction() {
+  console.log("I am the first function");
+}
 
-// function firstFunction() {
-//   console.log("I am the first function");
-// }
-//
-// function secondFunction() {
-//   firstFunction();
-//   console.log("I am the second function");
-//   function insideSecondFunction() {
-//     console.log("I am inside the second second function");
-//   }
-// }
-//
-// insideSecondFunction();
-// secondFunction();
+function secondFunction() {
+  firstFunction();
+  console.log("I am the second function");
+  function insideSecondFunction() {
+    console.log("I am inside the second second function");
+  }
+}
+// ---> can run
+secondFunction();
+// ---> Cannot run
+//insideSecondFunction();
 
-// function joinStringsAndPrint( string1 , string2 ) {
-//
-// 	var newString =  string1 + string2;
-//
-// 	function printString( string ) {
-//
-// 		console.log(string);
+// more examples
+function joinStringsAndPrint( string1 , string2 ) {
 
-		// Q3
-		// console.log(newString);
+	var newString =  string1 + string2;
 
-		// Q4
-		// console.log(string1 + string2);
+	function printString( string ) {
 
-// 	}
-//
-// 	printString( newString );
-//
-// 	return newString;
-//
-//
-// }
-// var string1 = "hello, ";
-// var string2 = "world";
-// joinStringsAndPrint( string1 , string2 );
+		console.log(string);
 
-// Q1
+		// Q3 ---> Can Run
+		console.log(newString);
+
+		// Q4 ---> Can run
+		console.log(string1 + string2);
+
+	}
+	printString( newString );
+	return newString;
+}
+
+var string1 = "hello, ";
+var string2 = "world";
+joinStringsAndPrint( string1 , string2 );
+
+//Q1 ---> Cannot run
 // printString( "run from outside joinStrings" );
-//
 
-// Q2
+
+//Q2 ---> Cannot run
 // console.log(newString)
